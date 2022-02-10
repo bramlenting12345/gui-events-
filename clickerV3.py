179f36d451dfc6fe84c267149634a5edcbc86fe2
bramlenting1234 <99068035@mydavinci.nl>
2022-02-10 14:23:17 +0100

    clickerV3_klaar

--- clickerV3.py
+++ clickerV3.py
@@ -9,23 +9,31 @@ import tkinter as tk
 
 opteller = [0]
 ORGINAL_COLLOR = "grey"
-lastClick = None
+last_click = None
+
+# ...............................................................................
 
 def optellen_up(opteller):
+    global last_click
     opteller[0] = opteller[0] + 1
     teller.configure(text=opteller[0])
+    last_click = "up"
 
     opteller_up = opteller[0]
     if opteller_up > 0:
         clicker.configure(bg="green")
+
     if opteller_up < 0:
         clicker.configure(bg="red")
     
+# ...................................................................................
 
 def afteller_down(opteller):
+    global last_click
     opteller[0] = opteller[0] - 1
     teller.configure(text=opteller[0])
     opteller_up = opteller[0]
+    last_click = "down"
     
     if opteller_up > 0:
         clicker.configure(bg="green")
@@ -33,25 +41,39 @@ def afteller_down(opteller):
     if opteller_up < 0:
         clicker.configure(bg="red")
 
+# ........................................................................................
     
 def verranderen_geel(event):
     clicker["background"] = "yellow"
 
-def verranderen_grijs(event):
+# .........................................................................................
+
+def verranderen_vorrige_kleur(event):
     kleur_switch = opteller[0]
     if kleur_switch > 0:
         clicker["background"] = "green"
+
     if kleur_switch < 0 :
         clicker["background"] = "red"    
 
+# .............................................................................................
 
-    
+def vervoudig_label(event):
+    global last_click
+    if last_click == "up":
+        teller.configure(text=opteller[0] * 3)
 
+    if last_click == "down":
+        teller.configure(text=opteller[0] / 3)
+
+# ...............................................................................................
 
 clicker=tk.Tk() 
 clicker.title("place() methode") 
 clicker.geometry("300x300")
 clicker.configure(bg="grey")
+
+# ................................................................................................
 
 button_up=tk.Button(clicker, text="UP",padx=80,pady=2,command=lambda:optellen_up(opteller))
 button_up.place(x=60, 
@@ -61,16 +83,23 @@ button_down=tk.Button(clicker, text="down",padx=80,pady=2,command=lambda:aftelle
 button_down.place(x=50, 
                   y=220,)                
 
+# ...................................................................................................
+
 teller = tk.Label(clicker,bg="white",text=opteller,padx=85,pady=2)
 teller.place(relx = 0.5,
              rely = 0.5,
              anchor = 'center')
 
+# .....................................................................................................
 
 teller.bind("<Double-Button-1 >",vervoudig_label)
 
+# ......................................................................................................
+
 teller.bind("<Enter>",verranderen_geel)
-teller.bind("<Leave>",verranderen_grijs)
+teller.bind("<Leave>",verranderen_vorrige_kleur)
+
+# ......................................................................................................
 
 clicker.mainloop()
 
--- clickerV3fout.py
+++ clickerV3fout.py
@@ -1,6 +1,9 @@
 from cgitb import text
 from logging import root
+from sre_parse import State
+from telnetlib import STATUS
 import tkinter as tk
+from turtle import bgcolor, up
 
 # Kopieer het programma Clicker v1 in deze repository en update deze met de volgende specificaties:
 
@@ -9,11 +12,16 @@ import tkinter as tk
 
 opteller = [0]
 ORGINAL_COLLOR = "grey"
-lastClick = None
 
-def optellen_up(opteller):
+
+# .............................................................................................
+
+def optellers_up(opteller):
     opteller[0] = opteller[0] + 1
     teller.configure(text=opteller[0])
+    if opteller > 0:
+        teller.configure(text=opteller_up[0] * 3)
+
 
     opteller_up = opteller[0]
     if opteller_up > 0:
@@ -21,56 +29,68 @@ def optellen_up(opteller):
     if opteller_up < 0:
         clicker.configure(bg="red")
     
+    
+# ...............................................................................................
 
 def afteller_down(opteller):
     opteller[0] = opteller[0] - 1
     teller.configure(text=opteller[0])
     opteller_up = opteller[0]
     
+    
     if opteller_up > 0:
         clicker.configure(bg="green")
-
     if opteller_up < 0:
         clicker.configure(bg="red")
 
     
+# ...............................................................................................
+
 def verranderen_geel(event):
     clicker["background"] = "yellow"
 
 def verranderen_grijs(event):
-    kleur_switch = opteller[0]
-    if kleur_switch > 0:
-        clicker["background"] = "green"
-    if kleur_switch < 0 :
-        clicker["background"] = "red"    
+    clicker["background"] = ORGINAL_COLLOR
 
-
-    
-
+# ................................................................................................
 
 clicker=tk.Tk() 
 clicker.title("place() methode") 
 clicker.geometry("300x300")
 clicker.configure(bg="grey")
 
-button_up=tk.Button(clicker, text="UP",padx=80,pady=2,command=lambda:optellen_up(opteller))
+# ..................................................................................................
+
+button_up=tk.Button(clicker, text="UP",padx=80,pady=2,command=lambda:optellers_up(opteller))
 button_up.place(x=60, 
                 y=40,)
 
-button_down=tk.Button(clicker, text="down",padx=80,pady=2,command=lambda:afteller_down(opteller))
+
+
+button_down=tk.Button(clicker, text="down",padx=80,pady=2,state = ["normal"],command=lambda:afteller_down(opteller))
 button_down.place(x=50, 
                   y=220,)                
+
+#......................................................................................................
 
 teller = tk.Label(clicker,bg="white",text=opteller,padx=85,pady=2)
 teller.place(relx = 0.5,
              rely = 0.5,
              anchor = 'center')
 
+teller.bind("<Double-Button-1 >",optellers_up(opteller))
 
-teller.bind("<Double-Button-1 >",vervoudig_label)
+#..................................................................................
+
+#...................................................................................
+
+
+#.....................................................................................
 
 teller.bind("<Enter>",verranderen_geel)
 teller.bind("<Leave>",verranderen_grijs)
+
+# .....................................................................................
 
 clicker.mainloop()
 
